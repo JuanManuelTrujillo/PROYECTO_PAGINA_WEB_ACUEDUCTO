ab9951c976c1faefe60cfb952aa162877a6efdf6
Yulieth <yfacundo2731@gmail.com>
2023-10-31 13:24:02 -0500

    Informacion Nueva

--- nuestra_empresa/funciones-generales.php
+++ nuestra_empresa/funciones-generales.php
@@ -126,10 +126,10 @@
         <!-- Navbar End -->
 
 
-                      <!-- Page Header Start -->
-                      <div class="container-fluid page-header py-5">
-            <div class="container text-center py-5">
-                <h1 class="display-2 text-white mb-4 animated slideInDown">Funciones Generales</h1>
+        <!-- Page Header Start -->
+              <div class="container-fluid page-header py-5">
+              <div class="container text-center py-5">
+              <h1 class="display-2 text-white mb-4 animated slideInDown">Funciones Generales</h1>
 
             </div>
         </div>

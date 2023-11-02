8eca247e085778e6652e58ad6e74490fc0f30bc7
Juan_Manuel_Trujillo <Juanperdomotorre.13@gmail.com>
2023-11-01 19:52:45 -0500

    fusion de ramas

--- index.php
+++ index.php
@@ -202,7 +202,7 @@
                     </div>
                     <div class="col-md-6 text-center text-md-end">
                         <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        <span class="text-light">Diseñado Por <a href="https://adsosena.com/" target="_blank" class="text-secondary">ADSO 2502629 </a>CADPH Garzón Huila</span>
+                        <span class="text-light">Diseñado Por <a href="https://adsosena.com/" target="_blank" class="text-secondary">ADSO 2502629 </a>CADPH Garzón Huila.</span>
 
                     </div>
                 </div>

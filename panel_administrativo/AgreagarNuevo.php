6a2289988c87db9a60fcb0e7e2681b8015973ffe
Yulieth <yfacundo2731@gmail.com>
2023-11-15 17:43:32 -0500

    Canales

--- panel_administrativo/AgreagarNuevo.php
+++ panel_administrativo/AgreagarNuevo.php
@@ -1,15 +1,8 @@
-<?php require_once "parte_superior.php"?>
- <!-- Content Wrapper. Contains page content -->
- <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-  <h1 class="mm-5 text-center">CANALES DE ATENCION</h1>
-<!-- INICIO DE CONTENIDO -->
 <?php
 
 	error_reporting( ~E_NOTICE ); // avoid notice
 	
-	require_once 'conexion_canal.php';
+	require_once 'Conexion.php';
 	
 	if(isset($_POST['btnsave']))
 	{
@@ -91,10 +84,14 @@
 <script src="bootstrap/js/jquery.min.js"></script>
 </head>
 <body>
-
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header"> <a class="navbar-brand" href="index.php" title='Inicio' target="_blank">Inicio</a> </div>
+  </div>
+</div>
 <div class="container">
   <div class="page-header">
-    <h1 class="h3 center">Cambio de Información </h1>
+    <h1 class="h3">Obras Acueducto. <a class="btn btn-default" href="index.php"> <span class="glyphicon glyphicon-eye-open"></span> &nbsp; Mostrar todo </a></h1>
   </div>
   <?php
 	if(isset($errMSG)){
@@ -123,7 +120,7 @@
         <td><input class="input-group" type="file" name="user_image" accept="image/*" /></td>
       </tr>
       <tr>
-        <td colspan="2"><button type="submit" name="btnsave" class="btn btn-success"> <span class="glyphicon glyphicon-save"></span> &nbsp; Guardar Imagen </button></td>
+        <td colspan="2"><button type="submit" name="btnsave" class="btn btn-default"> <span class="glyphicon glyphicon-save"></span> &nbsp; Guardar Imagen </button></td>
       </tr>
     </table>
   </form>
@@ -134,88 +131,3 @@
 <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
-<?php
-// Archivo de conexion con la base de datos
-require_once 'conexion_canal.php';
-// Condicional para validar el borrado de la imagen
-if(isset($_GET['delete_id']))
-{
-	// Selecciona imagen a borrar
-	$stmt_select = $DB_con->prepare('SELECT Imagen_Img FROM tbl_imagenes WHERE Imagen_ID =:uid');
-	$stmt_select->execute(array(':uid'=>$_GET['delete_id']));
-	$imgRow=$stmt_select->fetch(PDO::FETCH_ASSOC);
-	// Ruta de la imagen
-	unlink("imagenes/".$imgRow['Imagen_Img']);
-	
-	// Consulta para eliminar el registro de la base de datos
-	$stmt_delete = $DB_con->prepare('DELETE FROM tbl_imagenes WHERE Imagen_ID =:uid');
-	$stmt_delete->bindParam(':uid',$_GET['delete_id']);
-	$stmt_delete->execute();
-	// Redireccioa al inicio
-	header("Location: index.php");
-}
-
-?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=yes" />
-<title>Subir imagen </title>
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
-<script src="bootstrap/js/jquery.min.js"></script>
-</head>
-
-<body>
-
-<div class="container">
-  <div class="page-header">
-   
-  </div>
-  <br/>
-  <div class="row">
-    <?php
-	
-	$stmt = $DB_con->prepare('SELECT Imagen_ID, Imagen_Marca, Imagen_Tipo, Imagen_Img FROM tbl_imagenes ORDER BY Imagen_ID DESC');
-	$stmt->execute();
-	
-	if($stmt->rowCount() > 0)
-	{
-		while($row=$stmt->fetch(PDO::FETCH_ASSOC))
-		{
-			extract($row);
-			?>
-    <div class="col-xs-3">
-      <p class="page-header"><?php echo $Imagen_Marca."&nbsp;/&nbsp;".$Imagen_Tipo; ?></p>
-      <img src="imagenes/<?php echo $row['Imagen_Img']; ?>" class="img-rounded" width="250px" height="250px" />
-      <p class="page-header"> <span> <a class="btn btn-info" href="editar_canales.php?edit_id=<?php echo $row['Imagen_ID']; ?>" title="click for edit" onclick="return confirm('Esta seguro de editar el archivo ?')"><span class="glyphicon glyphicon-edit"></span> Editar</a> <a class="btn btn-danger" href="?delete_id=<?php echo $row['Imagen_ID']; ?>" title="click for delete" onclick="return confirm('Esta seguro de eliminar el archivo?')"><span class="glyphicon glyphicon-remove-circle"></span> Borrar</a> </span> </p>
-    </div>
-    <?php
-		}
-	}
-	else
-	{
-		?>
-    <div class="col-xs-12">
-      <div class="alert alert-warning"> <span class="glyphicon glyphicon-info-sign"></span> &nbsp; Datos no encontrados ... </div>
-    </div>
-    <?php
-	}
-	
-?>
-  </div>
- 
-</div>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
-  <!-- FIN DE CONTENIDO -->
-  </section>
-  <!-- /.content -->
-</div>
-
-
-</body>
-</html>
-  <?php require_once "parte_inferior.php"?>

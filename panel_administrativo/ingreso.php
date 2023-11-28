b61eaa80507611e48022726b80e40df46429d795
unknown <Administrador@GARAPRCADFSD638.FormacionSena.RED>
2023-11-28 16:48:19 -0500

    ingreso

--- panel_administrativo/ingreso.php
+++ panel_administrativo/ingreso.php
@@ -18,7 +18,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="index.html"><b>Acueducto</b> La Jagua Huila</a>
+    <a href="index.html"><b>Acueducto</b> La Jagua</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
